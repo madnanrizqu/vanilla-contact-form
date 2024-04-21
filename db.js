2fb54ccccdd2ea028a03b453245e6ed161eafbb4
Muhammad Adnan R <madnanrizqullah@gmail.com>
2024-04-21 08:11:45 +0300

    feat: implement local storage

--- db.js
+++ db.js
@@ -1,47 +1,47 @@
 const db = {
-  data: [
-    {
-      id: 1,
-      name: "Muhammad Adnan Rizqullah",
-      phone: "+6285156562419",
-      email: "madnanrizqullah@gmail.com",
-      address: "Jl Jatimulya No. 15",
-      age: 26,
-    },
-  ],
+  data: (newData) => {
+    if (newData) {
+      localStorage.setItem("data", JSON.stringify(newData));
+      return newData;
+    }
+
+    return localStorage.getItem("data")
+      ? JSON.parse(localStorage.getItem("data"))
+      : [];
+  },
   getContact(id) {
-    return this.data.find((v) => {
-      console.log(v);
-      console.log(v.id === id);
+    return this.data().find((v) => {
       return v.id === id;
     });
   },
   getAll() {
-    return this.data;
+    return this.data();
   },
   count() {
-    return this.data.length;
+    return this.data().length;
   },
   newId() {
-    return this.data.length + 1;
+    return this.data().length + 1;
   },
   append(contact) {
-    this.data = [...this.data, contact];
+    this.data([...this.data(), contact]);
   },
   update(id, newContact) {
-    this.data = this.data.map((v) => {
-      if (v.id === id) {
-        return {
-          id,
-          ...newContact,
-        };
-      } else {
-        return v;
-      }
-    });
+    this.data(
+      this.data().map((v) => {
+        if (v.id === id) {
+          return {
+            id,
+            ...newContact,
+          };
+        } else {
+          return v;
+        }
+      })
+    );
   },
   delete(id) {
-    const index = this.data.findIndex((v) => v.id === id);
-    this.data.splice(index, 1);
+    const index = this.data().findIndex((v) => v.id === id);
+    this.data().splice(index, 1);
   },
 };
